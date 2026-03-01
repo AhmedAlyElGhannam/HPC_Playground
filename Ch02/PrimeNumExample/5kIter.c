4fb8f89e09705703182e72e1fc35595943ca578f
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2026-03-02 00:45:19 +0200

    added ch02 example

---
+++ Ch02/PrimeNumExample/5kIter.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef enum 
+{
+    true = 0x01,
+    false = 0x00,
+} bool;
+
+int main(void)
+{
+    long long int x = 27644437;
+    int i, j;
+    bool isPrime = true;
+
+    for (i = 2, j = 1; i < sqrt(x); i++, j++)
+    {
+        printf("Iteration number %d\n", j);
+        if (x % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    
+    printf("x is prime? %d", isPrime);
+
+    return 0;
+}
---
+++ Ch02/PrimeNumExample/README.md
@@ -0,0 +1,10 @@
+# Results
+
+## Full Time
+![](./README_Photos/trash.png)
+
+## Slightly Optimized Time
+![](./README_Photos/ish.png)
+
+## Fully Optimized Time
+![](./README_Photos/super.png)
